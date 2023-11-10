f16069842b29fbc7bb8ab7214b574f023d1926a5
zoom <asmaaradi0@gmail.com>
2023-11-10 14:09:50 +0200

    java

---
+++ ran.js
@@ -0,0 +1,6 @@
+let arr=["ahmed","sayed","ali"];
+console.log(arr.length);
+console.log(Math.random()* arr.length)
+console.log(Math.floor(Math.random()* arr.length))
+console.log(Math.trunc(Math.random()* arr.length))
+console.log(arr[Math.trunc(Math.random()* arr.length)])
